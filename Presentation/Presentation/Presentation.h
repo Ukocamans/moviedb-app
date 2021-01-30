0d94231ff862cdb4ff17220bd9cec1c221a9aa44
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2021-01-30 08:30:11 +0300

    Add Presentation module

---
+++ Presentation/Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by Umurcan Kocaman on 30.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
