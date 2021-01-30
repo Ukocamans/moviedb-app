ba4fab6f71fcf92b20d5ac3c61c938b710e67c33
Umurcan Kocaman <kocaman.umurcan@gmail.com>
2021-01-30 08:32:09 +0300

    Add Data module

---
+++ Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Umurcan Kocaman on 30.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
